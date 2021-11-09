bfa75ec6766b26ffb64a389afdb51b85fee4ea5a
xyqlx <saint.diana@outlook.com>
2021-11-09 16:06:39 +0800

    support proxy & fix async bug

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const playwright = require('playwright');
+const config = require('./config.json');
 
 function extract_time(text) {
     const match = text.match(/((?<hour>\d+):)?(?<minute>\d+):(?<second>\d+)/);
@@ -7,14 +8,20 @@ function extract_time(text) {
         return parseInt(groups.hour ?? '0') * 3600 + parseInt(groups.minute) * 60 + parseInt(groups.second);
     }
     return 0;
-}(async () => {
+};
+(async () => {
     const browserType = playwright.firefox;
-    const browser = await browserType.launch({headless: true});
+    const launchConfig = {
+        headless: true
+    };
+    if (config.proxy) {
+        launchConfig.proxy = config.proxy;
+    }
+    const browser = await browserType.launch(launchConfig);
     const context = await browser.newContext();
     const page = await context.newPage();
     const videoList = ['BV1ah411t7Pp'];
-    videoList.forEach(videoId => {
-        // 这里应该监听事件/request/文字变化更稳一些可惜xyq不知道怎么做
+    for await(const videoId of videoList) { // 这里应该监听事件/request/文字变化更稳一些可惜xyq不知道怎么做
         await page.goto(`https://www.bilibili.com/video/${videoId}`);
         await page.waitForSelector('span.tit', {timeout: 10000});
         const titleSpan = await page.$('span.tit');
@@ -46,6 +53,6 @@ function extract_time(text) {
             process.stdout.write(`${currentTimeText}/${fullTimeText}\r`);
             await page.waitForTimeout(1000);
         }
-    })
-    await browser.close();
-})();
+    }
+
+await browser.close();})();
