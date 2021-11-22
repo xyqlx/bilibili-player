9e6703179879c542811fd0bf004ba19858445618
xyqlx <saint.diana@outlook.com>
2021-11-22 16:48:13 +0800

    修改了config加载方式，现在不需要代理的话可以不用建立config.json了

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 const playwright = require('playwright');
 const commandLineArgs = require('command-line-args')
 import * as readline from 'readline';
-import {stdin as input, stdout as output, stdout} from 'process';
-const config = require('./config.json');
-import {open_live, play_videos, search_videos, show_up_videos} from './bilibili_page.js'
+import { stdin as input, stdout as output, stdout } from 'process';
+import { open_live, play_videos, search_videos, show_up_videos } from './bilibili_page.js'
+const fs = require('fs');
 
+let config: {proxy?: {}} = {};
+try {
+    const rawdata = fs.readFileSync('config.json');
+    config = JSON.parse(rawdata);
+}
+catch { }
 
 const optionDefinitions = [
     {
@@ -17,7 +23,7 @@ const optionDefinitions = [
         alias: 's',
         type: String,
         multiple: true
-    },{
+    }, {
         name: 'debug',
         alias: 'd',
         type: Boolean,
@@ -30,17 +36,19 @@ const optionDefinitions = [
 const options = commandLineArgs(optionDefinitions);
 (async () => {
     const browserType = playwright.firefox;
-    const launchConfig: {headless:boolean,
-        proxy?:string} = {
-            headless: !options.debug,
-        };
+    let launchConfig: {
+        headless: boolean,
+        proxy?: {}
+    } = {
+        headless: !options.debug,
+    };
     if (config.proxy) {
         launchConfig.proxy = config.proxy;
     }
     const browser = await browserType.launch(launchConfig);
     const context = await browser.newContext();
     const page = await context.newPage();
-    let searchList: {id: string, title: string}[] = [];
+    let searchList: { id: string, title: string }[] = [];
     if (options.search && options.search.length > 0) { //
         searchList = await search_videos(page, options.search);
         searchList.forEach((video, index) => {
@@ -49,12 +57,12 @@ const options = commandLineArgs(optionDefinitions);
     } else if (options.live) {
         const liveId = options.live.match(/(?=https:\/\/live\.bilibili\.com\/)?\d+/)[0];
         await open_live(page, liveId);
-    } else if (options.playlist ?. length ?? 0 > 0) {
-        const videoList = options.playlist.filter((x : string) => x).map((x : string) => x !.match(/[AaBb][Vv]\w+/)).filter((x : RegExpMatchArray) => x && x.length > 0).map((x : RegExpMatchArray) => x[0])
+    } else if (options.playlist?.length ?? 0 > 0) {
+        const videoList = options.playlist.filter((x: string) => x).map((x: string) => x!.match(/[AaBb][Vv]\w+/)).filter((x: RegExpMatchArray) => x && x.length > 0).map((x: RegExpMatchArray) => x[0])
         await play_videos(page, videoList);
     } else { // 进入命令模式
         output.write('COMMAND MODE\n');
-        const rl = readline.createInterface({input, output});
+        const rl = readline.createInterface({ input, output });
         while (true) {
             const inputText: string = await new Promise(resolve => {
                 rl.question('', resolve)
@@ -64,11 +72,11 @@ const options = commandLineArgs(optionDefinitions);
             splits.splice(0, 1);
             // play
             if (command.startsWith('p')) {
-                if(splits.length == 0){
-                    await play_videos(page, searchList.map(x=>x.id));
-                }else if(splits[0].match(/^\d{1,2}$/)){
+                if (splits.length == 0) {
+                    await play_videos(page, searchList.map(x => x.id));
+                } else if (splits[0].match(/^\d{1,2}$/)) {
                     await play_videos(page, [searchList[parseInt(splits[0])].id]);
-                }else{
+                } else {
                     await play_videos(page, splits)
                 }
             }
@@ -102,7 +110,7 @@ const options = commandLineArgs(optionDefinitions);
                 break;
             }
             // new
-            else if(command.startsWith('n')){
+            else if (command.startsWith('n')) {
                 searchList = await show_up_videos(page, splits[0], true);
                 searchList.forEach((video, index) => {
                     process.stdout.write(`${video.id} ${index} ${video.title}\n`);
